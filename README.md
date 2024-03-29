1dd506d290762acc4e3c25aa9191f5397b2d67c4
mathena828 <mathena828@gmail.com>
2024-03-30 03:03:59 +0800

    Removed google command and added roll command

---
+++ README.md
@@ -0,0 +1,6 @@
+## Getting Started
+1. Navigate inside to the repository.
+2. Run `pip install requirements.txt`.
+3. Create a `.env` file with in the parent directory.
+4. Add `TOKEN=<INSERT TOKEN HERE>` to the `.env` file.
+5. Run `python3 bot.py`.
--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ import requests
 import discord
 import asyncio
 import random
-from requests_html import HTMLSession
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -21,12 +20,16 @@ async def on_ready():
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CheckFailure):
-        await ctx.send(f'I encountered an error. Don\'t worry, Master {ctx.author.id}. Things always get worse before they get better."')
+        await ctx.send(f'I encountered an error. Don\'t worry, Master {ctx.author.mention}. Things always get worse before they get better."')
 
 @bot.command(name="set_timer", help='Sets a timer for the specified number of minutes')
-async def set_timer(ctx, minutes: int):
+async def set_timer(ctx, minutes: float):
+    timer = minutes
     seconds = minutes * 60
-    message = await ctx.send(f'Timer: {seconds//60} minutes left')
+    if minutes == 1:
+        message = await ctx.send(f'{ctx.author.mention} set a timer for {timer} minute.')
+    else:
+        message = await ctx.send(f'{ctx.author.mention} set a timer for {timer} minutes.')
     while seconds>0:
         seconds -= 1
         if seconds%60 == 0:
@@ -37,7 +40,8 @@ async def set_timer(ctx, minutes: int):
                 await message.edit(content=f'Timer: {seconds//60} minute left')
         await asyncio.sleep(1)
     await message.edit(content=f'Timer: 0 minutes left')
-    await ctx.send(content=f'The timer {ctx.author.name} set for {minutes} minutes has ended.')
+    await ctx.send(content=f'The timer {ctx.author.mention} set for {timer} minutes has ended.')
+
 
 @bot.command(name='magic_8_ball', help='Simulates a Magic 8 Ball')
 async def magic_eight_ball(ctx):
@@ -46,7 +50,7 @@ async def magic_eight_ball(ctx):
         'Don\'t count on it.',
         'As I see it, yes.',
         'You can rely on it.',
-        'Ask again later',
+        'Ask me again later.',
         'My sources say no.',
         'The outlook is not so good.',
         'Very doubtful...',
@@ -57,24 +61,8 @@ async def magic_eight_ball(ctx):
     response = random.choice(magic_eight_ball_answers)
     await ctx.send(response)
        
-@bot.command(name="google", help='Returns 1 search result from Google')
-async def google(ctx, *args):
-    google_url = 'https://google.com/search?q='
-    for arg in args:
-        google_url+=f"+{arg}"
-    try:
-        session = HTMLSession()
-        response = session.get(google_url)
-        links = list(response.html.absolute_links)
-        results = []
-        for link in links:
-            if 'google' not in link:
-                results.append(link)
-        if len(results)>0:
-            await ctx.send(results[0])
-        else:
-            await ctx.send(f'Apologies, Master {ctx.author.id}, I could not find anything on Google.')
-    except requests.exceptions.RequestException as exception:
-        print(exception)
+@bot.command(name="roll", help='Rolls a 6-sided dice')
+async def roll(ctx):
+    await ctx.send(f'{ctx.author.mention} rolls a {random.randint(1,6)}.')
 
 bot.run(TOKEN)
